aa1dcae24ba9c41df50a8be07369ca4a36041ed8
RookieWangZhiWei <RookieWangZhiWei@163.com>
2018-06-08 22:58:20 +0800

    commit

--- taotao-manager/taotao-manager-interface/src/main/java/com/taotao/service/ItemService.java
+++ taotao-manager/taotao-manager-interface/src/main/java/com/taotao/service/ItemService.java
@@ -1,11 +1,14 @@
 package com.taotao.service;
 
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.pojo.TbItem;
 
 public interface ItemService {
 	public TbItem getItemById(Long itemId) ;
 	
 	public EasyUIDataGridResult getItemList(int page,int rows);
+	
+	public TaotaoResult addItem(TbItem item,String desc);
 	
 }
--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -6,6 +6,7 @@ package com.taotao.service.impl;
  *
  */
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,39 +15,76 @@ import org.springframework.stereotype.Service;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.IDUtils;
+import com.taotao.mapper.TbItemDescMapper;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemDesc;
 import com.taotao.pojo.TbItemExample;
 import com.taotao.service.ItemService;
 
 @Service
-public class ItemServiceImpl implements ItemService{
-	
-	
+public class ItemServiceImpl implements ItemService {
+
 	@Autowired
 	private TbItemMapper itemMapper;
-	
-	
+
+	@Autowired
+	private TbItemDescMapper itemDescMapper;
+
 	@Override
 	public TbItem getItemById(Long itemId) {
 		TbItem item = itemMapper.selectByPrimaryKey(itemId);
 		return item;
 	}
 
-
 	@Override
 	public EasyUIDataGridResult getItemList(int page, int rows) {
-		//设置分页信息
+		// TODO Auto-generated method stub
 		PageHelper.startPage(page, rows);
-		//执行查询
+
 		TbItemExample example = new TbItemExample();
+
 		List<TbItem> list = itemMapper.selectByExample(example);
-		//取查询结果
+
 		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+
 		EasyUIDataGridResult result = new EasyUIDataGridResult();
+
 		result.setRows(list);
+
 		result.setTotal(pageInfo.getTotal());
-		//返回结果
+
 		return result;
 	}
+
+	@Override
+	public TaotaoResult addItem(TbItem item, String desc) {
+		// TODO Auto-generated method stub
+		long itemId = IDUtils.getItemId();
+
+		item.setId(itemId);
+
+		item.setStatus((byte) 1);
+
+		item.setCreated(new Date());
+
+		item.setUpdated(new Date());
+
+		itemMapper.insert(item);
+
+		TbItemDesc itemDesc = new TbItemDesc();
+
+		itemDesc.setItemId(itemId);
+		itemDesc.setItemDesc(desc);
+		itemDesc.setUpdated(new Date());
+		itemDesc.setCreated(new Date());
+
+		itemDescMapper.insert(itemDesc);
+
+		return TaotaoResult.ok();
+
+	}
+
 }
